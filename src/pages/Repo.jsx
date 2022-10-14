aad7e42fa8795882701e83e0b4098127bd8a1fd9
octaviodesalvatore <octaviodesalvatore@gmail.com>
2022-10-14 19:13:52 -0300

    update repo page

--- src/pages/Repo.jsx
+++ src/pages/Repo.jsx
@@ -1,12 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import RepoContext from "../context/RepoContext";
 
 const Repo = () => {
   const { saveUrl } = useContext(RepoContext);
+
+  const [content, setContent] = useState([]);
+  console.log("hola");
+
+  useEffect(() => {
+    fetch(`${saveUrl}/contents`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setContent(() => data);
+      })
+      .catch((err) => {
+        // setIsLoading(false);
+        // setError(true);
+        console.error(err);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>{saveUrl}</h1>
-    </div>
+    <>
+      <table>
+        <tr>
+          <th>Nombre</th>
+          <br />
+          <th>Tipo</th>
+        </tr>
+        {content.map((contenido) => (
+          <tr>
+            <td>{contenido.path}</td>
+            <br />
+            <br />
+            <td>{contenido.type}</td>
+          </tr>
+        ))}
+      </table>
+    </>
   );
 };
 
