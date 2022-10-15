226664f51224475fa680c713e8eb60e35f4c255e
octaviodesalvatore <octaviodesalvatore@gmail.com>
2022-10-15 01:09:49 -0300

    navigation and back

--- src/pages/Repo.jsx
+++ src/pages/Repo.jsx
@@ -8,16 +8,25 @@ const Repo = () => {
   const [currentUrl, setCurrentUrl] = useState(`${saveUrl}/contents`);
   const [urlBefore, setUrlBefore] = useState("");
 
+  console.log(atob(""));
+
+  console.log(currentUrl);
+
   const updateUrl = (url) => {
-    setCurrentUrl(`${currentUrl}/${url}`);
+    setCurrentUrl(`${currentUrl}${url}`);
   };
 
   const openInNewTab = (url) => {
     window.open(url, "_blank", "noopener,noreferrer");
   };
 
+  console.log(process.env.REACT_APP_GITHUB_API);
   useEffect(() => {
-    fetch(currentUrl)
+    fetch(currentUrl, {
+      headers: {
+        Authorization: `${process.env.REACT_APP_GITHUB_API}`,
+      },
+    })
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -30,9 +39,18 @@ const Repo = () => {
       });
   }, [currentUrl]);
 
+  console.log("mi contenido", content);
+
   return (
     <>
-      <button>volver</button>
+      <button
+        onClick={() => {
+          setCurrentUrl(currentUrl.slice(0, currentUrl.lastIndexOf("/")));
+          console.log(updateUrl);
+        }}
+      >
+        volver
+      </button>
       <table>
         <tr>
           <th>Nombre</th>
@@ -44,10 +62,11 @@ const Repo = () => {
             <td
               onClick={() => {
                 if (contenido.type === "dir") {
-                  updateUrl(contenido.path);
+                  updateUrl(`/${contenido.name}`);
                   console.log("carpeta");
                 } else if (contenido.type === "file") {
                   openInNewTab(`${contenido.download_url}`);
+
                   console.log("archivo");
                 }
               }}
