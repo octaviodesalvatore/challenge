791ff920793e29eb04ea8a9bb5c1323fcd6e94a9
octaviodesalvatore <octaviodesalvatore@gmail.com>
2022-10-17 08:56:56 -0300

    ui button back

---
+++ src/Components/ButtonBack.jsx
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import RepoContext from "../context/RepoContext";
+import { useNavigate } from "react-router-dom";
+
+const ButtonBack = () => {
+  const navigate = useNavigate();
+
+  const { currentUrl, setclickedComponent, updateUrl, urlFromHome } =
+    useContext(RepoContext);
+
+  const checkUrl = (url) => {
+    if (urlFromHome === url) {
+      navigate("/");
+      window.location.reload(false);
+    }
+  };
+
+  return (
+    <ButtonB
+      onClick={() => {
+        setclickedComponent(false);
+        //no se llega actualizar
+        let url = currentUrl.slice(0, currentUrl.lastIndexOf("/"));
+        updateUrl(url);
+        checkUrl(url);
+      }}
+    >
+      Back
+    </ButtonB>
+  );
+};
+
+export default ButtonBack;
+
+const ButtonB = styled.button`
+  border: none;
+  padding: 10px 18px;
+  border-radius: 5px;
+`;
--- src/Components/Repo.jsx
+++ src/Components/Repo.jsx
@@ -1,42 +1,18 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import File from "./File";
 import RepoContext from "../context/RepoContext";
 import Table from "./Table";
-import { useNavigate } from "react-router-dom";
+import ButtonBack from "./ButtonBack";
 import styled from "styled-components";
 
 const Repo = () => {
-  const navigate = useNavigate();
-  const {
-    currentUrl,
-    clickedComponent,
-    content,
-    setclickedComponent,
-    updateUrl,
-    urlFromHome,
-  } = useContext(RepoContext);
-
-  const checkUrl = (url) => {
-    if (urlFromHome === url) {
-      navigate("/");
-      window.location.reload(false);
-    }
-  };
+  const { clickedComponent, content } = useContext(RepoContext);
 
   return (
     <RepoContainer>
-      <button
-        onClick={() => {
-          setclickedComponent(false);
-          //no se llega actualizar
-          let url = currentUrl.slice(0, currentUrl.lastIndexOf("/"));
-          updateUrl(url);
-          checkUrl(url);
-        }}
-      >
-        volver
-      </button>
-
+      <div>
+        <ButtonBack />
+      </div>
       {content && clickedComponent ? <File content={content} /> : <Table />}
     </RepoContainer>
   );
@@ -44,4 +20,8 @@ const Repo = () => {
 
 export default Repo;
 
-const RepoContainer = styled.div``;
+const RepoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+`;
