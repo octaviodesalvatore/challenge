79d58524039775bd5dd7de59d9d06f9971ebaab7
octaviodesalvatore <octaviodesalvatore@gmail.com>
2022-10-17 17:28:04 -0300

    code syntax

--- src/Components/File.jsx
+++ src/Components/File.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const File = ({ content }) => {
   const fileVeri = (file) => {
@@ -10,13 +12,29 @@ const File = ({ content }) => {
     if (name.includes(".svg"))
       return <div dangerouslySetInnerHTML={{ __html: atob(file.content) }} />;
 
-    return <code>{atob(`${content.content}`)}</code>;
+    const decodeCode = atob(`${content.content}`);
+
+    const languages = {
+      ".js": "javascript",
+      ".css": "css",
+      ".html": "html",
+    };
+
+    return (
+      <SyntaxHighlighter
+        className="codeBox"
+        language={languages[name.substring(name.lastIndexOf("."))]}
+        style={atomOneDarkReasonable}
+      >
+        {decodeCode}
+      </SyntaxHighlighter>
+    );
   };
 
   return (
     <FileContainer>
+      <h3>{content.name}</h3>
       <div className="Filebox">
-        <h3>{content.name}</h3>
         <p>{content.content && fileVeri(content)}</p>
       </div>
     </FileContainer>
@@ -27,14 +45,24 @@ export default File;
 
 const FileContainer = styled.div`
   .Filebox {
-    background-color: #0d1117;
+    background-color: #000000;
+    min-width: 1000px;
     padding: 20px;
-    border-radius: 10px;
+    border-end-end-radius: 10px;
+    border-end-start-radius: 10px;
+  }
+
+  .codeBox {
+    background-color: #000000 !important;
   }
 
   h3 {
     color: #ffffff;
-    margin-bottom: 20px;
+    background-color: #022833;
+    font-weight: 900;
+    padding: 20px;
+    border-start-start-radius: 10px;
+    border-start-end-radius: 10px;
   }
   p {
     color: #ffffff;
